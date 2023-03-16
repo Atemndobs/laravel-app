6c57a7d2968e03886e2a12878600a4bc381126ea
Atemndobs <atemndobs@yahoo.com>
2023-03-16 21:53:45 +0100

    update db deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,7 @@
 
 git clone https://github.com/Atemndobs/laravel-app.git /var/www/html/larvel-app
 cp -rf /var/www/html/larvel-app/* /var/www/html
-composer install --ignore-platform-reqs --optimize-autoloader --no-dev
-composer require --dev phpunitgen/console
+composer install --ignore-platform-reqs --optimize-autoloader --no-interaction
 sh /var/www/html/scripts/db_init.sh
 
 
--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- /usr/bin/mysql --host=mariadb --port=3306 -uroot -pDHjCkvFGRf mysql -e";
+ /usr/bin/mysql --host=mariadb-galera.curator.svc.cluster.local --port=3306 -uroot -pdBcvts63Qd mysql -e";
 ALTER USER 'root'@'%' IDENTIFIED BY 'mage';FLUSH PRIVILEGES;
 DROP user IF EXISTS 'mage'@'%';
 FLUSH PRIVILEGES;
@@ -10,5 +10,5 @@ FLUSH PRIVILEGES;
 SET character_set_server = utf8;
 SET collation_server = utf8mb3_general_ci;
 "
- /usr/bin/mysql --user=mage -pmage --host=mariadb --port=3306 -e "CREATE SCHEMA mage;"
+ /usr/bin/mysql --user=mage -pmage --host=mariadb-galera.curator.svc.cluster.local --port=3306 -e "CREATE SCHEMA mage;"
  /usr/bin/mysql --database=mage --user=mage -pmage --host=mariadb --port=3306 <  /var/www/html/storage/app/backups/latest/db-dumps/mysql-mage.sql
