81ab6ae625cfc30c422e6f12182dbf34184b5a30
Atemndobs <atemndobs@yahoo.com>
2023-03-16 16:40:12 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# We don't install the packages at runtime. This is done when building the Docker image.
+# The reason behind this is to be able to serve the container as fast as possible,
+# without adding overhead to the scaling process.
+
+# This file is only for small minor fixes on the project, like deploying the files to the CDN,
+# caching the config, route, view, running migrations, etc.
+
+git clone https://github.com/Atemndobs/laravel-app.git /var/www/html/larvel-app
+cp -rf /var/www/html/larvel-app/* /var/www/html
+composer install --ignore-platform-reqs --optimize-autoloader --no-dev
+
+#php artisan config:cache
+#php artisan route:cache
+#php artisan view:cache
