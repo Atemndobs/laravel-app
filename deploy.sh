d0ea03644991d2d73477fc3cbd9eb720c99165eb
Atemndobs <atemndobs@yahoo.com>
2023-03-17 09:36:32 +0100

    update db init script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@
 # This file is only for small minor fixes on the project, like deploying the files to the CDN,
 # caching the config, route, view, running migrations, etc.
 
-git clone https://github.com/Atemndobs/laravel-app.git /var/www/html/larvel-app
-cp -rf /var/www/html/larvel-app/* /var/www/html
 composer install --ignore-platform-reqs --optimize-autoloader --no-interaction
 sh /var/www/html/scripts/db_init.sh
 
