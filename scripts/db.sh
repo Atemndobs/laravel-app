39b0a41e6baff91de5f023eb576fb7cb5e35033b
Atemndobs <atemndobs@yahoo.com>
2023-03-25 15:16:55 +0100

    debut minio

--- scripts/db.sh
+++ scripts/db.sh
@@ -112,4 +112,29 @@ start slave;
 
 "
 
-# /usr/bin/mysql --database=mage --user=mage -pmage --host=127.0.0.1 --port=4000 <  /home/atem/sites/curator/laravel/storage/app/backups/2022-08-22-21-02-03/db-dumps/mysql-mage.sql
+***********************************************************************
+ /usr/bin/mysql --host=mariadb-galera.curator.svc.cluster.local --port=3306 -uroot -pmage mysql -e";
+ALTER USER 'root'@'%' IDENTIFIED BY 'mage';FLUSH PRIVILEGES;
+DROP user IF EXISTS 'mage'@'%';
+FLUSH PRIVILEGES;
+CREATE USER 'mage'@'%' IDENTIFIED BY 'mage';
+GRANT ALL PRIVILEGES ON * . * TO 'mage'@'%';
+FLUSH PRIVILEGES;
+SET character_set_server = utf8;
+SET collation_server = utf8mb3_general_ci;
+"
+
+
+ /usr/bin/mysql --database=mage --user=mage -pmage --host=mariadb-galera --port=3306 <  /var/www/html/storage/app/backups/latest/db-dumps/mysql-mage.sql
+
+echo "Patch MeiliSearch"
+sh scripts/patch_meilisearch.sh
+
+echo "Update Paths, images and related_songs for Songs"
+php artisan song:path -f related_songs -i mage --dry-run
+
+php artisan song:path -d music
+php artisan song:path -d images
+php artisan song:path -f related_songs -i mage
+
+echo "Update Paths, images and related_songs for Songs"
