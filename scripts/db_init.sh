c947a4fcd19cc483b09ff2660bbb87ffb72772f1
Atemndobs <atemndobs@yahoo.com>
2023-03-25 15:40:40 +0100

    debug minio

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp patches/AwsS3V3Adapter.php vendor/laravel/framework/src/Illuminate/Filesystem/AwsS3V3Adapter.php
+cp patches/FilesystemAdapter.php  vendor/laravel/framework/src/Illuminate/Filesystem/FilesystemAdapter.php
