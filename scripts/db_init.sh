43b3eff983fa0e0c3b6e0d9671b096f318ad5b83
Atemndobs <atemndobs@yahoo.com>
2023-03-25 16:05:20 +0100

    debug minio

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-echo "Patch MeiliSearch"
-sh scripts/patch_meilisearch.sh
+
 
 echo "Update Paths, images and related_songs for Songs"
 php artisan song:path -f related_songs -i mage --dry-run
+
+echo "remove vendor and composer.lock"
 rm -rf vendor
 rm -f composer.lock
+
+echo "Patch MeiliSearch"
+sh scripts/patch_meilisearch.sh
+
+echo "composer install"
 composer install
