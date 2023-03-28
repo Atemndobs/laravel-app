641e962d6a71a01a98c950f1b139335d77fa921f
Atemndobs <atemndobs@yahoo.com>
2023-03-28 23:29:07 +0200

    debug

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
-echo "Update Paths, images and related_songs for Songs"
-php artisan song:path -f related_songs -i mage --dry-run
-
-echo "remove vendor and composer.lock"
-rm -rf vendor
-rm -f composer.lock
-
-echo "composer install"
-composer install
+#
+#echo "Update Paths, images and related_songs for Songs"
+#php artisan song:path -f related_songs -i mage --dry-run
+#
+#echo "remove vendor and composer.lock"
+#rm -rf vendor
+#rm -f composer.lock
+#
+#echo "composer install"
+#composer install
 
 #echo "Patch MeiliSearch"
 #sh scripts/patch_meilisearch.sh
