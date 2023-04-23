1e150efd14a5388fa9f819c955ea704069ac2b0e
Atemndobs <atemndobs@yahoo.com>
2023-04-23 10:14:48 +0200

    update db dump

--- scripts/db_init_local.sh
+++ scripts/db_init_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- /usr/bin/mysql --user=root -pmage --host=127.0.0.1 --port=4000 -e";
+/usr/bin/docker exec -it mariadb_master_1 mysql -uroot -pmage mysql -e";
 ALTER USER 'root'@'%' IDENTIFIED BY 'mage';FLUSH PRIVILEGES;
 DROP user IF EXISTS 'mage'@'%';
 FLUSH PRIVILEGES;
@@ -10,16 +10,18 @@ FLUSH PRIVILEGES;
 SET character_set_server = utf8;
 SET collation_server = utf8mb3_general_ci;
 "
+
+
+
 echo "DB initialized"
- /usr/bin/mysql --user=mage -pmage --host=127.0.0.1 --port=4000 -e "CREATE SCHEMA IF NOT EXISTS mage;"
- /usr/bin/mysql --database=mage --user=mage -pmage --host=127.0.0.1 --port=4000 <  ~/sites/curator/laravel/storage/app/backups/latest/db-dumps/mysql-mage.sql
+/usr/bin/mysql --user=mage -pmage --host=127.0.0.1 --port=4000 -e "CREATE SCHEMA IF NOT EXISTS mage;"
+/usr/bin/mysql --database=mage --user=mage -pmage --host=127.0.0.1 --port=4000 <  ~/sites/curator/laravel/storage/app/backups/latest/db-dumps/mysql-mage.sql
 echo "DB successfully initialized with backups/latest/db-dumps/mysql-mage.sql"
 
-echo "Update Paths, images and related_songs for Songs"
-mariadb_slave_1
-
-a song:path -d music
-a song:path -d images
-a song:path -f related_songs -i mage
-
-echo "Update Paths, images and related_songs for Songs"
+#echo "Update Paths, images and related_songs for Songs"
+#
+#a song:path -d music
+#a song:path -d images
+#a song:path -f related_songs -i mage
+#
+#echo "Update Paths, images and related_songs for Songs"
