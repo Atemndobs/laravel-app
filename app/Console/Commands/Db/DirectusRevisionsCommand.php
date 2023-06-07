6f231959fec011988a5c22056d1852d1d18ee023
Atemndobs <atemndobs@yahoo.com>
2023-06-07 11:22:19 +0200

    add command to clear directus revisions

--- app/Console/Commands/Db/DirectusRevisionsCommand.php
+++ app/Console/Commands/Db/DirectusRevisionsCommand.php
@@ -26,12 +26,14 @@ class DirectusRevisionsCommand extends Command
      */
     public function handle()
     {
+        // get count of all records in the directus_revisions table
+        $count = \DB::table('directus_revisions')->count();
         // get the directus_revisions table in database and truncate it
         try {
             \DB::table('directus_revisions')->truncate();
             $message = [
                 'message' => 'Directus revisions table truncated',
-                'action' => 'success'
+                'action' => 'delete ' . $count . ' records'
             ];
         } catch (\Exception $e) {
             $message = [
