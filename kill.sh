52395d592757476058ab5f5202b1a7ff6433d7a3
Atemndobs <atemndobs@yahoo.com>
2023-06-11 11:05:12 +0200

    update schedulers

---
+++ kill.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run the command and extract the PID
+pid=$(ps aux | grep "php artisan schedule:work" | grep -v grep | awk '{print $2}')
+
+# Check if the PID is empty
+if [ -z "$pid" ]; then
+  echo "No matching process found."
+else
+  echo "Killing process with PID: $pid"
+  # Kill the process using the obtained PID
+  kill -9 "$pid"
+fi
