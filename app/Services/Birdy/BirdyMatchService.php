dd512ea6dc5fa5ffbdcc220469930eaaf09b808f
Atemndobs <atemndobs@yahoo.com>
2023-07-31 16:04:59 +0200

    clear match critera

--- app/Services/Birdy/BirdyMatchService.php
+++ app/Services/Birdy/BirdyMatchService.php
@@ -495,7 +495,7 @@ class BirdyMatchService
         } else {
             $filter[] = "slug != '{$song->slug}'";
         }
-        return $filter;
+        return array_unique($filter);
     }
 
     /**
