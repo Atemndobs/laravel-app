88c9dee6b7079f37aa5510dd206689403f9bf16a
Atemndobs <atemndobs@yahoo.com>
2023-12-25 16:46:26 +0100

    update song link paths

--- app/Console/Commands/ListFixer.php
+++ app/Console/Commands/ListFixer.php
@@ -23,7 +23,7 @@ class ListFixer extends Command
      *
      * @var string
      */
-    protected $description = 'Fix List';
+    protected $description = 'Fix Song links to point to aws s3 bucket';
 
     /**
      * Execute the console command.
@@ -32,28 +32,37 @@ class ListFixer extends Command
      */
     public function handle()
     {
-        $path = $this->option('path');
-        $this->info('Fixing List...');
-
-        $files = glob($path . '/*');
-
-        foreach ($files as $file) {
-            $fileName = basename($file);
-            $fileName = str_replace('.mp3', '', $fileName);
-            $fileName = Str::slug($fileName, '_');
-            $fileName = $fileName . '.mp3';
-            // if file name changed, save new file back to original path
-            if ($fileName !== basename($file)) {
-                $destination = $path . '/' . $fileName;
-                dump([
-                    'file' => basename($file),
-                    'fileName' => $fileName,
-                    'destination' => $destination,
-                ]);
-                rename($file, $destination);
+        // get all songs that are not on aws s3 bucket and update them. these songs have a path starting with http://s3.atemkeng.de:9000
+        $allSongs = Song::query()->get();
+        $this->info("Found {$allSongs->count()} songs in the database");
+        $songs = Song::query()->where('path', 'like', 'http://s3.atemkeng.de:9000%')->get();
+        // update the path to point to the aws s3 bucket in the format "https://s3.amazonaws.com/curators3/music/" +  $song->slug
+        $count = $songs->count();
+        $fixedSongs = [];
+        $this->info("Found $count songs to fix");
+        // progress bar start
+        $bar = $this->output->createProgressBar(count($songs));
+        $songs->each(function ($song) use (&$fixedSongs, $bar) {
+            // if slug ens with mp3, remove it
+            if (Str::endsWith($song->slug, 'mp3')) {
+                $song->slug = Str::replaceLast('mp3', '', $song->slug);
             }
-        }
-
-        dd('done !!!!!');
+            $song->path = "https://s3.amazonaws.com/curators3/music/" . $song->slug . '.mp3';
+            $song->save();
+            $fixedSongs[] = $song->slug;
+            $countFixed = count($fixedSongs);
+            $this->info("Fixed $countFixed songs");
+            $this->warn("Fixed {$song->slug}");
+            $bar->advance();
+            dd('PAUSE');
+        });
+        $bar->finish();
+        $message = [
+            'fixedSongs' => count($fixedSongs),
+           // 'fixedSongsList' => $fixedSongs,
+            'totalSongs' => $count,
+        ];
+        $this->info(json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ));
+        $this->info('==============Done================');
     }
 }
