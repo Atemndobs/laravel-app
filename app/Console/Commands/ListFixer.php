270cd36c78066e38a93914760a43f4bfc6d1d392
Atemndobs <atemndobs@yahoo.com>
2023-12-28 21:42:58 +0100

    update image  paths

--- app/Console/Commands/ListFixer.php
+++ app/Console/Commands/ListFixer.php
@@ -16,7 +16,7 @@ class ListFixer extends Command
      *
      * @var string
      */
-    protected $signature = 'fix {--p|path=}';
+    protected $signature = 'fix {--p|path=} {--s|source=}';
 
     /**
      * The console command description.
@@ -32,18 +32,35 @@ class ListFixer extends Command
      */
     public function handle()
     {
-        // get all songs that are not on aws s3 bucket and update them. these songs have a path starting with http://s3.atemkeng.de:9000
+       $this->fixImages();
+
+       dd();
+    
+       $path = $this->option('path') ?? 'music';
+       $source = $this->option('source') ?? 'audio';
+       $this->uploadToAwsS3($path, $source);
+       return 0;
+    }
+
+    /**
+     * Fix the songs by updating their paths to point to the AWS S3 bucket.
+     *
+     * @return void
+     */
+    private function fixSongs()
+    {
+        // get all songs that are not on AWS S3 bucket and update them. These songs have a path starting with http://s3.atemkeng.de:9000
         $allSongs = Song::query()->get();
         $this->info("Found {$allSongs->count()} songs in the database");
         $songs = Song::query()->where('path', 'like', 'http://s3.atemkeng.de:9000%')->get();
-        // update the path to point to the aws s3 bucket in the format "https://s3.amazonaws.com/curators3/music/" +  $song->slug
+        // update the path to point to the AWS S3 bucket in the format "https://s3.amazonaws.com/curators3/music/" +  $song->slug
         $count = $songs->count();
         $fixedSongs = [];
         $this->info("Found $count songs to fix");
         // progress bar start
         $bar = $this->output->createProgressBar(count($songs));
         $songs->each(function ($song) use (&$fixedSongs, $bar) {
-            // if slug ens with mp3, remove it
+            // if slug ends with mp3, remove it
             if (Str::endsWith($song->slug, 'mp3')) {
                 $song->slug = Str::replaceLast('mp3', '', $song->slug);
             }
@@ -58,10 +75,76 @@ class ListFixer extends Command
         $bar->finish();
         $message = [
             'fixedSongs' => count($fixedSongs),
-           // 'fixedSongsList' => $fixedSongs,
             'totalSongs' => $count,
         ];
         $this->info(json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ));
         $this->info('==============Done================');
     }
+
+
+    private function uploadToAwsS3($dir = 'music', $source = 'audio')
+    {
+        $files = Storage::disk('public')->files($source);
+        $count = count($files);
+        $this->info("Found $count files");
+        $bar = $this->output->createProgressBar(count($files));
+        $slugs = [];
+        foreach ($files as $file) {
+            // make the files this format: /var/www/html/storage/app/public/audio/015b_not_bad_feat_dawon.mp3
+            $file = '/var/www/html/storage/app/public/' . $file;
+            // upload to aws s3
+            $this->info("Uploading $file to aws s3");
+            // call s3:put {--f|file=} {--d|directory=music}
+            $this->call('s3:put', [
+                '--file' => $file,
+                '--directory' => $dir
+            ]);
+
+            $slugs[] = $file;   
+            $bar->advance();
+        }
+        $bar->finish();
+        $this->info("Found " . count($slugs) . " slugs");
+        $message = [
+            'slugsList' => $slugs,
+            'totalFiles' => $count,
+        ];
+        $this->info(json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ));
+    }
+
+    //function to fix image links
+    private function fixImages()
+    {
+        $songs = Song::query()->where('image', 'like', 'http://s3.atemkeng.de:9000%')->get();
+        $count = $songs->count();
+        $fixedSongs = [];
+        $this->info("Found $count songs to fix");
+        // progress bar start
+        $bar = $this->output->createProgressBar(count($songs));
+        $songs->each(function ($song) use (&$fixedSongs, $bar) {
+            if (Str::startsWith($song->image, 'https://i.scdn.co/image/')) {
+                $bar->advance();
+                return;
+            }
+
+            // $file_name = last part of song->image
+            $file_name = last(explode('/', $song->image));
+            $song_image = "https://s3.amazonaws.com/curators3/images/" . $file_name ;
+            $song->image = $song_image;
+            $song->save();
+            $fixedSongs[] = $song->slug;
+            $countFixed = count($fixedSongs);
+            $this->info("Fixed $countFixed songs");
+            $this->warn("Fixed {$song->slug}");
+            $bar->advance();
+        });
+        $bar->finish();
+        $message = [
+            'fixedSongs' => count($fixedSongs),
+            'totalSongs' => $count,
+        ];
+        $this->info(json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ));
+        $this->info('==============Done================');
+    }
+    
 }
