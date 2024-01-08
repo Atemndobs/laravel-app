817f152900fc1877998e5c4dd9f6433fb16de1bf
Atemndobs <atemndobs@yahoo.com>
2024-01-08 11:49:58 +0100

    fix audio

--- app/Console/Commands/Song/SongAudioFixCommand.php
+++ app/Console/Commands/Song/SongAudioFixCommand.php
@@ -15,7 +15,7 @@ class SongAudioFixCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'audio:fix  {--p|path=} {--a|all} {--d|dry-run} {--b|batch=} {--f|file=}';
+    protected $signature = 'audio:fix  {--p|path=} {--a|all} {--d|dry-run} {--b|batch=} {--f|file=} {--s|skip=}';
 
     /**
      * The console command description.
@@ -37,10 +37,56 @@ class SongAudioFixCommand extends Command
         $dryRun = $this->option('dry-run');
         $batch = $this->option('batch');
         $file = $this->option('file');
+        $skip = $this->option('skip');
         // if path is not provided, create a folder in root directory called fixed
         if ($path === null) {
             $path = 'fixed';
         }
+        if ($batch === null) {
+            $batch = 100;
+        }
+//        if ($file === null) {
+//            $file = 'songsWithAudio.txt';
+//        }
+//        if ($skip === null) {
+//            $skip = 'songsWithoutAudio.txt';
+//        }
+
+        // If file is given then read the file and get the songs from the file using the slug. All these songs should have audio therefore they should be skipped
+
+        $songs = Song::query()->get();
+        if ($file !== null) {
+            $songsWithAudio = file_get_contents($file);
+            $songsWithAudio = explode("\n", $songsWithAudio);
+            $songsWithAudio = array_filter($songsWithAudio);
+            $songsWithAudio = array_unique($songsWithAudio);
+            $songsWithAudioCount = count($songsWithAudio);
+            $this->info("Found $songsWithAudioCount songs with audio");
+            $songs = Song::query()->whereNotIn('slug', $songsWithAudio)->get();
+            $songsCount = $songs->count();
+            $this->info("Found $songsCount songs to fix");
+           // return 0;
+        }
+
+
+
+        // if skip is given then read the file and get the songs from the file using the slug
+        if ($skip !== null) {
+            $songsWithoutAudio = file_get_contents($skip);
+            $songsWithoutAudio = explode("\n", $songsWithoutAudio);
+            $songsWithoutAudio = array_filter($songsWithoutAudio);
+            $songsWithoutAudio = array_map('trim', $songsWithoutAudio);
+            $songsWithoutAudio = array_unique($songsWithoutAudio);
+            $songsWithoutAudioCount = count($songsWithoutAudio);
+            $this->info("Found $songsWithoutAudioCount songs without audio");
+            $songs = Song::query()->whereIn('slug', $songsWithoutAudio)->get();
+            $songsCount = $songs->count();
+            $this->info("Found $songsCount songs to fix");
+            return 0;
+        }
+
+
+
 
         // log out the full path
         $fullPath = base_path($path);
@@ -48,10 +94,12 @@ class SongAudioFixCommand extends Command
         $songsWithAudio = [];
         $songsWithoutAudio = [];
         // get all songs
-        $songs = Song::query()->get();
-        $songsCount = Song::query()->count();
+        $totalSongs = Song::query()->get();
+        $totalSongsCount = Song::query()->count();
+        $songsCount = count($songs);
 
         $info = [
+            'totalSongsCount' => $songsCount,
             'songsCount' => $songsCount,
             'batch' => $batch,
             'path' => $path,
@@ -60,7 +108,13 @@ class SongAudioFixCommand extends Command
         // for all songs, check if the path url is working in batches of 100
 
 
+        dump([
+            'Songs To check' => $songsCount,
+        ]);
+        $bar = $this->output->createProgressBar(count($songs));
+        $bar->start();
         foreach ($songs as $song) {
+            $bar->advance();
             $url = $song->path;
             $this->info("Checking $url");
             $response = Http::get($url);
@@ -80,7 +134,15 @@ class SongAudioFixCommand extends Command
                 fwrite($file, $song->slug . "\n");
                 fclose($file);
             }
+            // output songs left to check
+            $songsLeft = $songsCount - count($songsWithAudio);
+            $this->line("<fg=yellow> $songsLeft songs left to check</>");
+            if ($songsLeft === 0) {
+                $this->info("All songs have been checked");
+                break;
+            }
         }
+        $bar->finish();
         // log out the number of songs with audio and without audio
         $songsWithAudioCount = count($songsWithAudio);
         $songsWithoutAudioCount = $songsCount - $songsWithAudioCount;
