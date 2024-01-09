4e345ae82d5ff6be4bb3e5baf53c77d5eae7e1a7
Atemndobs <atemndobs@yahoo.com>
2024-01-09 16:52:29 +0100

    remove duplicates

--- app/Console/Commands/Song/SongCleanUpCommand.php
+++ app/Console/Commands/Song/SongCleanUpCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\Song;
 use App\Models\Song;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 use Stancl\Tenancy\Events\DatabaseDeleted;
 
 class SongCleanUpCommand extends Command
@@ -30,27 +31,81 @@ class SongCleanUpCommand extends Command
      */
     public function handle()
     {
-        $deletableSongs = Song::query()->whereNot('path', 'like', '%http%')->get();
-        $totalSongs = Song::query()->count();
-        $this->info("Total Songs: $totalSongs");
-//        if ($deletableSongs->count() <= 0) {
-//            $this->info('No songs to delete');
-//            return 0;
-//        }
-        // progress bar start
-//        $bar = $this->output->createProgressBar(count($deletableSongs));
-//        $deletableSongs->each(function ($song) use ($bar) {
-//            $song->delete();
-//            $this->output->info("$song->title : deleted");
-//            $bar->advance();
-//        });
-//        $bar->finish();
 
-        // Find all duplicate songs and delete them (Duplicate songs are songs with the same slug)
+        // get all songs files from the s3 bucket music folder
+        $uploadedSongs = Storage::disk('s3')->files('music');
+        $uploadedSongsCount = count($uploadedSongs);
+        $this->info("Found {$uploadedSongsCount} songs in the s3 bucket");
+
+        // Extract the slug from the songs in the s3 bucket and
+        $uploadedSongsSlugs = [];
+        foreach ($uploadedSongs as $uploadedSong) {
+            $uploadedSongSlug = str_replace('music/', '', $uploadedSong);
+            $uploadedSongSlug = str_replace('.mp3', '', $uploadedSongSlug);
+            $uploadedSongsSlugs[] = $uploadedSongSlug;
+        }
+        //check if they exist in the database
+        $songs = Song::query()->get();
+        $songsCount = $songs->count();
+        $this->info("Found {$songsCount} songs in the database");
+        $songsSlugs = $songs->pluck('slug')->toArray();
+        // get the difference between the songs in the database and the songs in the s3 bucket
+        $deletableSongs = array_diff($songsSlugs, $uploadedSongsSlugs);
+        $deletableSongsCount = count($deletableSongs);
+        $info = [
+            'songs in the database' => $songsCount,
+            'songs in the s3 bucket' => $uploadedSongsCount,
+            'songs to delete' => $deletableSongsCount,
+        ];
+        $infoMessage = json_encode($info, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        $this->line("<fg=yellow>$infoMessage</>");
+        // for each deletable file in the database, check if it is analyzed. If it is analyzed, keep it
+        $deletableSongs = Song::query()->whereIn('slug', $deletableSongs)->get();
+        foreach ($deletableSongs as $deletableSong) {
+            if ($deletableSong->analyzed) {
+                $this->info("Song $deletableSong->title is analyzed");
+                $deletableSong->status = 'duplicate';
+                $deletableSong->save();
+            } else {
+                $this->info("Song $deletableSong->title is not analyzed");
+                $deletableSong->status = 'deleted';
+            }
+        }
+        dd('END OF SCRIPT');
+        $this->markDuplicateSongs();
+        $bar = $this->output->createProgressBar(count($duplicateSongs));
+        if ($this->option('exist')) {
+            $this->info('Cleaning up songs that dont exist from the database');
+            // progress bar start
+            $bar = $this->output->createProgressBar(count($deletableSongs));
+            $songs = Song::all();
+            foreach ($songs as $song) {
+                $url = str_replace('mage.tech:8899', 'nginx', $song->path);
+                $filename = basename($url);
+                $this->info("checking $filename");
+                $fileExists = file_exists("/var/www/html/storage/app/public/audio/$filename");
+                if (!$fileExists) {
+                    $song->delete();
+                    $this->output->info("$song->title : deleted");
+                }
+
+                $bar->advance();
+            }
+
+        }
+        $bar->finish();
+        return 0;
+    }
+
+    /**
+     * @return \Symfony\Component\Console\Helper\ProgressBar
+     */
+    public function markDuplicateSongs(): \Symfony\Component\Console\Helper\ProgressBar
+    {
         $duplicateSongs = Song::query()->select('slug')->groupBy('slug')->havingRaw('count(*) > 1')->get();
         $this->info("Found {$duplicateSongs->count()} duplicate songs");
 
-       // dd($duplicateSongs);
+        // dd($duplicateSongs);
         $bar = $this->output->createProgressBar(count($duplicateSongs));
 
         dump(count($duplicateSongs));
@@ -78,7 +133,7 @@ class SongCleanUpCommand extends Command
                         $this->info(json_encode($messageWorking, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
                         return true;
                     }
-                   // return $song->path !== null && $song->path !== '';
+                    // return $song->path !== null && $song->path !== '';
                     return false;
                 });
                 if ($analyzedSongsWithWorkingPath->count() > 0) {
@@ -96,7 +151,7 @@ class SongCleanUpCommand extends Command
                     $this->info("Keeping song $songToKeep->title");
                     $songs->each(function ($song) use ($songToKeep) {
                         if ($song->id !== $songToKeep->id) {
-                          //  $song->delete();
+                            //  $song->delete();
                             $song->status = 'duplicate';
                             $song->save();
                             $messageDelete = [
@@ -146,33 +201,11 @@ class SongCleanUpCommand extends Command
                 });
             }
             $songs->each(function ($song) {
-               // $song->delete();
+                // $song->delete();
                 $this->output->info("$song->title : deleted");
             });
             $bar->advance();
         });
-
-        dd('END OF SCRIPT');
-        if ($this->option('exist')) {
-            $this->info('Cleaning up songs that dont exist from the database');
-            // progress bar start
-            $bar = $this->output->createProgressBar(count($deletableSongs));
-            $songs = Song::all();
-            foreach ($songs as $song) {
-                $url = str_replace('mage.tech:8899', 'nginx', $song->path);
-                $filename = basename($url);
-                $this->info("checking $filename");
-                $fileExists = file_exists("/var/www/html/storage/app/public/audio/$filename");
-                if (!$fileExists) {
-                    $song->delete();
-                    $this->output->info("$song->title : deleted");
-                }
-
-                $bar->advance();
-            }
-
-        }
-        $bar->finish();
-        return 0;
+        return $bar;
     }
 }
